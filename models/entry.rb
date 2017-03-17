40c6c0dc1e3e168d010c3c56cc2f8aed3d82076b
Andrew <andrewmbyrd@gmail.com>
2017-03-17 14:21:12 -0500

    updated method_missing

--- models/entry.rb
+++ models/entry.rb
@@ -6,13 +6,13 @@ class Entry < BlocRecord::Base
     "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
   end
 
-  #we're leaning heavily on the limitation that this only needs to work for the find_by method. So any attemtped method name
-  #will be find_by_name or find_by_email or find_by_attributethatalreadyexistsandisspelledcorrectly
-  #also args will be of length 1 and has the corresponding value in the attribute to be searched
+  #we're only implementing this for specifically 'update_name' as per assignment specification
   def method_missing(attempted_method_name, *args, &block)
-    #we know the desired attribute starts after the chars find_by_, so position 8
-    desired_attribute = attempted_method_name.slice(8, attempted_method_name.length - 1)
-    find_by(desired_attribute, args[0]) if desired_attribute.length > 1
+
+    if attempted_method_name == "update_name"
+      update = {"name" => args.first}
+      self.class.update(self.id, update)
+    end
   end
 
 end
